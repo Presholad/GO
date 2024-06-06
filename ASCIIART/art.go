7b7c270579398d5536e1322c2238d79b367efe66
Precious Oladimeji <preciousoladimeji@Preciouss-MacBook-Pro.local>
2024-06-06 09:57:34 -0700

    added ASCIIART

---
+++ ASCIIART/art.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	f "fmt"
+	t "time"
+)
+
+func main() {
+	f.Println("  --      -")
+	f.Println("o'')}____//")
+	f.Println(" `_/       )")
+	f.Println(" (_(_/-(_/")
+
+	f.Printf("%s\n", t.Now().String())
+
+	f.Println("    `.-::::::-.`    ")
+	f.Println(".:-::::::::::::::-:.")
+	f.Println("`_:::    ::    :::_`")
+	f.Println(" .:( ^   :: ^   ):. ")
+	f.Println(" `:::   (..)   :::. ")
+	f.Println(" `:::::::UU:::::::` ")
+	f.Println(" .::::::::::::::::. ")
+	f.Println(" O::::::::::::::::O ")
+	f.Println(" -::::::::::::::::- ")
+	f.Println(" `::::::::::::::::` ")
+	f.Println("  .::::::::::::::.  ")
+	f.Println("    oO:::::::Oo     ")
+	f.Printf(t.Now().String())
+
+}
